3ef38c078b66349b11a8f07081f685e31fb071d1
mariartc <maraki.rtc@hotmail.com>
2021-05-11 23:17:10 +0300

    Add information to choice boxes of main page

--- front-end/src/js/Choice.js
+++ front-end/src/js/Choice.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+import React from "react";
 
 export default function Choice(props) {
     return (
-        <div className='choice-box'>
-            <img src={props.icon} className='choice-icon'/>
-            <h2>{props.title}</h2>
-        </div>
+        <Link to={props.url}>
+            <div className='choice-box'>
+                <img src={props.icon} className='choice-icon'/>
+                <h2>{props.title}</h2>
+            </div>
+        </Link>
     )
 }
--- front-end/src/js/ChoicesBox.js
+++ front-end/src/js/ChoicesBox.js
@@ -4,10 +4,10 @@ import icon from "./keyword.png"
 export default function ChoicesBox() {
     return (
         <div className='choices-container'>
-            <Choice icon={icon} title="Questions per keyword"/>
-            <Choice title="Questions per day/period"/>
-            <Choice title="Ask a new question"/>
-            <Choice title="Answer a question"/>
+            <Choice icon={icon} title="Questions per keyword" url='./'/>
+            <Choice title="Questions per day/period" url='./'/>
+            <Choice title="Ask a new question" url='./askquestion'/>
+            <Choice title="Answer a question" url='./answerquestion'/>
         </div>
     )
 }
