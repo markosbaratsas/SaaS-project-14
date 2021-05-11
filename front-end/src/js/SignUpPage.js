a2bae317b0c81997ae1bfcce67128e0fec514d51
mariartc <maraki.rtc@hotmail.com>
2021-05-11 19:31:39 +0300

    S14-28 Further develop sign up / log in pages

---
+++ front-end/src/js/SignUpPage.js
@@ -0,0 +1,27 @@
+import Title from "./Title";
+import SignBox from "./SignBox";
+
+export default function SignUpPage(){
+    return (
+        <div className="total-wrapper">
+
+            <section>
+                <div className='wave wave1'/>
+                <div className='wave wave2'/>
+
+
+                <div className='main-page-body'>
+                    <div className='title-box'>
+                        <div className='title-box-vertical'>
+                            <Title text='This is the sign up page.'/>
+                            <h2 className='welcome-text'>Sign up</h2>
+                        </div>
+                    </div>
+                    <SignBox/>
+                </div>
+
+
+            </section>
+        </div>
+    )
+}
