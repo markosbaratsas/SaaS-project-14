e922ea8f8cd297bf98da06a7ecff56b4a728f488
Maria Retsa <73224956+mariartc@users.noreply.github.com>
2021-06-09 14:09:28 +0300

    S14-49 Implement get user answers per period endpoint

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -556,5 +556,48 @@ router.post('/get-user-questions-per-period/',
         )
     })
 
+router.post('/get-user-answers-per-period/',
+    passport.authenticate('token', { session: false }),
+    function(req, res, next) {
+        let user_id = req.user.id
+        let today = new Date()
+        let date_from = req.body['date_from'] ? (new Date(String(req.body['date_from']))) : new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7); //abstract one week to get last week's questions
+        let date_to = req.body['date_to'] ? (new Date(String(req.body['date_to']))) : today;
+        if (!(date_from >= 0)) {
+            res.status(400);
+            return res.json({error: "Please provide a valid date_from format..." });
+        }
+        if (!(date_to > 0)) {
+            res.status(400);
+            return res.json({error: "Please provide a valid date_to format..." });
+        }
+        let period = getDates(date_from, date_to)
+        pool.query(
+            `SELECT  count(*), to_char(DateAnswered,'YYYY-MM-DD') as date 
+                FROM answer
+                WHERE DateAnswered >= to_timestamp($1/ 1000.0) AND DateAnswered <= to_timestamp($2/ 1000.0) AND UserID = $3
+                GROUP BY date
+                ORDER BY count(*) DESC`,
+            [date_from.getTime(), date_to.getTime(), user_id],
+            (err, results) => {
+                if (err) {
+                    res.status(400);
+                    return res.json({error: "Something went wrong..."});
+                } else {
+                    let returnedDates = results.rows.map((row) => {
+                        return row['date']
+                    })
+                    let answer = []
+                    let j = 0
+                    for(let i = 0; i < period.length; i++) {
+                        if(returnedDates.includes(period[i])) answer.push({ date: period[i], count: results.rows[j++]['count'] })
+                        else answer.push({ date: period[i], count: 0 })
+                        if(i === period.length - 1) return res.json( { answers_per_period: answer });
+                    }
+                }
+            }
+        )
+    })
+
 
 module.exports = router;
