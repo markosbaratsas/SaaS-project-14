474b27f80c3af76e51d6bf593ca6e37969777ead
Markos Baratsas <el17010@mail.ntua.gr>
2021-07-10 15:31:33 +0300

    Implement redis for get questions per keyword microservice

--- Microservices/get-questions-per-keyword/routes/index.js
+++ Microservices/get-questions-per-keyword/routes/index.js
@@ -3,6 +3,30 @@ var router = express.Router();
 
 const { pool } = require("../config/database");
 
+const redis_pool = require('redis-connection-pool')('myRedisPool', {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+    }
+);
+
+redis_pool.hget('subscribers', 'create-question', async(err, data) => {
+    let currentSubscribers = JSON.parse(data);
+    let alreadySubscribed = false;
+
+    let myAddress = process.env.MY_ADDRESS;
+    for(let i = 0; i < currentSubscribers.length; i++) {
+        if (currentSubscribers[i] === myAddress)
+            alreadySubscribed = true;
+    }
+    if(alreadySubscribed === false){
+        currentSubscribers.push(myAddress);
+        redis_pool.hset('subscribers', 'create-question', JSON.stringify(currentSubscribers), () => {})
+        console.log("Subscribed");
+    }
+    else
+        console.log("Already subscribed")
+});
+
 router.get('/get-questions-per-keyword/',
     function(req, res, next) {
         pool.query(
@@ -15,6 +39,7 @@ router.get('/get-questions-per-keyword/',
                 }
                 else {
                     let answer = results.rows.map((row) => {
+                        console.log(row);
                         return {keyword: row['keyword'], count: row['count'] }
                     })
                     return res.json({ questions_per_keyword: answer });
@@ -22,5 +47,53 @@ router.get('/get-questions-per-keyword/',
             }
         )
     });
+
+
+router.post('/bus', (req, res) => {
+    let event = req.body.event;
+    let keywords = event['keywords'] ? event['keywords'] : [];
+
+    for (let i = 0; i < keywords.length; i++) {
+        pool.query(
+            `SELECT * FROM question_keyword WHERE keyword = $1`,
+            [keywords[i]],
+            (err, results) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500);
+                    res.json({ error: "Something went wrong..." });
+                }
+                else if(results.rows.length > 0) {
+                    pool.query(
+                        `UPDATE question_keyword
+                        SET count = $1
+                        WHERE keyword = $2`,
+                        [parseInt(results.rows[0]['count']) + 1, keywords[i]],
+                        (err, results) => {
+                            if (err) {
+                                console.log(err);
+                                res.status(500);
+                                res.json({error: "Something went wrong..."});
+                            } else if (i === (keywords.length - 1)) res.json({success: "Successfully increased count of date"});
+                        }
+                    )
+                } else {
+                    pool.query(`INSERT INTO question_keyword (keyword, count)
+                                VALUES ($1, 1)`,
+                        [keywords[i]],
+                        (err, results) => {
+                            if (err) {
+                                console.log(err);
+                                res.status(500);
+                                res.json({ error: "Something went wrong..." });
+                            }
+                            else if (i === (keywords.length - 1)) res.json({ success: "Successfully increased count of date" });
+                        }
+                    )
+                }
+            }
+        );
+    }
+})
 
 module.exports = router;
