6b5d33ff60060941ce6e565402e74211c6c6c114
Markos Baratsas <el17010@mail.ntua.gr>
2021-07-10 15:57:50 +0300

    Implement redis for get user questions microservice

--- Microservices/get-user-questions/routes/index.js
+++ Microservices/get-user-questions/routes/index.js
@@ -7,6 +7,30 @@ const ExtractJWT = require('passport-jwt').ExtractJwt;
 
 const { pool } = require("../config/database");
 
+const redis_pool = require('redis-connection-pool')('myRedisPool', {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+    }
+);
+
+redis_pool.hget('subscribers', 'create-question', async(err, data) => {
+    let currentSubscribers = JSON.parse(data);
+    let alreadySubscribed = false;
+
+    let myAddress = process.env.MY_ADDRESS;
+    for(let i = 0; i < currentSubscribers.length; i++) {
+        if (currentSubscribers[i] === myAddress)
+            alreadySubscribed = true;
+    }
+    if(alreadySubscribed === false){
+        currentSubscribers.push(myAddress);
+        redis_pool.hset('subscribers', 'create-question', JSON.stringify(currentSubscribers), () => {})
+        console.log("Subscribed");
+    }
+    else
+        console.log("Already subscribed")
+});
+
 passport.use('token', new JWTStrategy(
     {
         secretOrKey: process.env.JWT_SECRET,
@@ -40,5 +64,26 @@ router.get('/get-user-questions/',
             }
         )
     });
+
+router.post('/bus', (req, res) => {
+    let event = req.body.event;
+    let question_id = event['id'];
+    let question_title = event['title'];
+    let user_email = event['user_email'];
+
+    pool.query(
+        `INSERT INTO userquestions (question_id, question_title, user_email)
+         VALUES ($1, $2, $3)`,
+        [question_id, question_title, user_email],
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                res.status(500);
+                res.json({ error: "Something went wrong..." });
+            }
+            else res.json({ success: "Successfully inserted user question" });
+        }
+    );
+});
 
 module.exports = router;
