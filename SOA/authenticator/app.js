872fddca5c8b59f576478a20e19443e56a5f9b9b
Maria Retsa <73224956+mariartc@users.noreply.github.com>
2021-07-11 18:11:39 +0300

    Fix bug regarding cors policy of SOA services

--- SOA/authenticator/app.js
+++ SOA/authenticator/app.js
@@ -4,9 +4,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors')
 
-var usersRouter = require('./routes/users');
-
 var app = express();
+app.use(cors())
+
+var usersRouter = require('./routes/users');
 
 // view engine setup
 app.engine('pug', require('pug').__express)
@@ -18,7 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
 
 app.use('/', usersRouter);
 // catch 404 and forward to error handler
